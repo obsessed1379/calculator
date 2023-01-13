3c2f8d8a7b029a8a8416ebe0586ea8af456a0e73
Mohammad Pourmami <M.Pourmami@gmail.com>
2023-01-13 18:09:39 +0330

    add math things

--- script.js
+++ script.js
@@ -30,15 +30,50 @@
             this.operators.push(char)
             this.currentNumber = ""
         },
+        cosop:function(fNumber)
+        {
+            return Math.cos(fNumber * Math.PI / 180)
+        },
+        sin:function(fNumber)
+        {
+            return Math.sin(fNumber * Math.PI /180) 
+        },
+        radikal:function(fNumber)
+        {
+            return Math.sqrt(fNumber)
+        },
+        tanop:function(fNumber)
+        {
+            return Math.tan(fNumber)
+        },
+        cotanop:function(fNumber)
+        {
+            return 1 / Math.tan(fNumber)
+        },
+        factorial: function(fNumber)
+        {
+            if(fNumber === 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return fNumber * this.factorial(fNumber - 1);
+            }
+        },
         clear:function(){
             this.operators = this.numbers = []
-            this.currentNumber="0"
+            this.currentNumber='0'
+            this.numbers=[]
+            this.operators=[]
+            this.allNumbers=''
         },
         result: function(){
             this.numbers.push(this.currentNumber)
             let NumberLength = this.numbers.length
             let fNumber = this.numbers[0]
             let sNumber = 0
+            
             
             for(let i = 1; i < NumberLength; i++){
                 sNumber = this.numbers[i]
@@ -52,16 +87,35 @@
                         break;
                     case "*":
                         fNumber = parseFloat(fNumber) * parseFloat(sNumber)
-                    break;
+                        break;
                     case "/":
                         fNumber = parseFloat(fNumber) / parseFloat(sNumber)
-                    break;   
-                }
+                       break;   
+                    case "cos":
+                        fNumber = this.cosop(fNumber);
+                    break;  
+                    case "sin":
+                        fNumber = this.sin(fNumber);
+                    break;  
+                    case "√":
+                        fNumber = this.radikal(fNumber);
+                    break;  
+                    case "tan":
+                        fNumber = this.tanop(fNumber);
+                    break;
+                    case "cotan":
+                        fNumber = this.cotanop(fNumber);
+                    break;  
+                    case "!":
+                        fNumber = this.factorial(fNumber);
+                    break;
+                    }
             }
-
+            this.operators= []
             this.numbers = []
             this.allNumbers = ""
             this.currentNumber = fNumber
         }
+    
     }
 })
